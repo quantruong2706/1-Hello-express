7660d4aadf454db53645753693de54377f35ee00
quantruong2706 <truongilv3000@gmail.com>
2020-04-23 22:37:17 +0700

    Cookie

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -18,6 +18,7 @@ module.exports.search = function(req,res){
 };
 
 module.exports.create = function(req,res){
+	console.log(req.cookies);
 	res.render('users/create');
 };
 
--- routes/user.route.js
+++ routes/user.route.js
@@ -9,6 +9,11 @@ const validate = require('../validate/user.validate')
 
 router.get('/', controller.index);
 
+router.get('/cookie', function(req, res){
+	res.cookie('user-id', 12345);
+	res.send("Hello");
+});
+
 router.get('/search', controller.search);
 
 router.get('/create', controller.create);
