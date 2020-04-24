5a05efb12b435396baa72f367a1e6d60970c1144
quantruong2706 <truongilv3000@gmail.com>
2020-04-24 17:28:38 +0700

    signedCookies

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,3 +1,5 @@
+const md5 = require('md5');
+
 const db = require('../db');
 
 module.exports.login = (req,res) => res.render('auth/login',);	
@@ -8,26 +10,31 @@ module.exports.postLogin  = function(req, res){
 
 	var user = db.get('users').find({email: email}).value();
 
-	if(!user || user.password !== password){
+	var hashedPassword = md5(password);
+
+	if(!user){
 		res.render('auth/login', {
 			errors: [
-			'User dose not exist or Wrong password.'
+			'User dose not exist.'
 			],
 			values: req.body
 		});
 		return;
 	}
 
-	// if(user.password !== password){
-	// 	res.render('auth/login', {
-	// 		errors: [
-	// 		'Wrong password.'
-	// 		],
-	// 		values: req.body
-	// 	});
-	// 	return;
-	// }
+	if(user.password !== hashedPassword){
+		res.render('auth/login', {
+			errors: [
+			'Wrong password.'
+			],
+			values: req.body
+		});
+		return;
+	}
 
-	res.cookie('userId', user.id);
+	res.cookie('userId', user.id, {
+		signed: true
+	});
+	
 	res.redirect('/users')	;
 }
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.set('views', './views');
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use(cookieParser());
+app.use(cookieParser('asdaskjhsajckwc1234'));
 
 app.use(express.static('public'));
 
