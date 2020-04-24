062c2f2f72d11b6a60341b06b7520ddab5cf5844
quantruong2706 <truongilv3000@gmail.com>
2020-04-24 20:20:45 +0700

    dotenv

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+console.log(process.env.SESSION_SECRET);
+// console.log(process.env);
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -15,7 +19,7 @@ app.set('views', './views');
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use(cookieParser('asdaskjhsajckwc1234'));
+app.use(cookieParser(process.env.SESSION_SECRET));
 
 app.use(express.static('public'));
 
